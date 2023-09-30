fd556a346013f8a483cc826dc56fc771d058ed60
Mohamed Nizar <nizarp666@gmail.com>
2023-09-30 20:55:08 +0530

    firebase configuration updation

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,72 @@
+import React,{useContext} from 'react';
+import {useHistory,Link} from 'react-router-dom'
+import './Header.css';
+import OlxLogo from '../../assets/OlxLogo';
+import Search from '../../assets/Search';
+import Arrow from '../../assets/Arrow';
+import SellButton from '../../assets/SellButton';
+import SellButtonPlus from '../../assets/SellButtonPlus';
+
+
+import { AuthContext,FirebaseContext } from '../../store/FirebaseContext';
+
+
+function Header() {
+  const {user}=useContext(AuthContext)
+  const {firebase}=useContext(FirebaseContext)
+  const history=useHistory()
+  return (
+    <div className="headerParentDiv">
+      <div className="headerChildDiv">
+        <div className="brandName">
+          <OlxLogo></OlxLogo>
+        </div>
+        <div className="placeSearch">
+          <Search></Search>
+          <input type="text" />
+          <Arrow></Arrow>
+        </div>
+        <div className="productSearch">
+          <div className="input">
+            <input
+              type="text"
+              placeholder="Find car,mobile phone and more..."
+            />
+          </div>
+          <div className="searchAction">
+            <Search color="#ffffff"></Search>
+          </div>
+        </div>
+        <div className="language">
+          <span> ENGLISH </span>
+          <Arrow></Arrow>
+        </div>
+        {user ?
+ <span>{user.displayName}</span>
+  : 
+  <Link to='/login'>    <div className="loginPage">
+  <span>Login</span>
+  <hr />
+</div>
+</Link> 
+}
+        {user &&<span onClick={()=>{
+          firebase.auth().signOut();
+          history.push('/login')
+        }} >Logout</span>  }
+{ user &&
+     <Link to='/create'>  <div className="sellMenu">
+          <SellButton></SellButton>
+          <div className="sellMenuContent">
+            <SellButtonPlus></SellButtonPlus>
+            <span>SELL</span>
+          </div>
+        </div>
+        </Link> 
+}
+      </div>
+    </div>
+  );
+}
+
+export default Header;
